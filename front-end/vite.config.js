d672a47c8e2d89958e4a090b5da336943b5a1af7
panditshukl <panditashushukl@gmail.com>
2025-09-22 11:35:52 +0530

    Refactor Vite configuration to support dynamic API URL based on environment

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,20 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://todos-mern-wb64.onrender.com',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-  
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const targetUrl =
+    mode === "production"
+      ? env.VITE_PROD_URL
+      : env.VITE_DEV_URL || "http://localhost:3000";
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: targetUrl,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 });
